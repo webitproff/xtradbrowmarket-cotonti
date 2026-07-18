163789f8372b622af430dd0a3252e6308f986e13
webitproff <webitproff@gmail.com>
2026-07-19 02:49:03 +0300

    Update xtradbrowmarket.extrafields.php

--- xtradbrowmarket/xtradbrowmarket.extrafields.php
+++ xtradbrowmarket/xtradbrowmarket.extrafields.php
@@ -8,13 +8,22 @@
 /**
  * xtradbrowmarket Plugin
  *
- * Date: Apr 27Th, 2026
+ * Filename: plugins/xtradbrowmarket/xtradbrowmarket.extrafields.php
+ *
+ * Custom Extrafields Market i18n plugin for Cotonti v1.+, PHP 8.4+, MySQL 8.4 
+ *
+ * ReadMeMore:       https://abuyfile.com/ru/market/cotonti/plugs/extrafields-market-custom 
+ * Support:          https://abuyfile.com/ru/forums/cotonti/original/extrafields
+ * API Extrafields:  https://github.com/Cotonti/Cotonti/blob/master/system/extrafields.php
+ *
+ * Date: Jul 18, 2026
  * @package xtradbrowmarket
- * @version 2.7.9
+ * @version 3.0.0
  * @author webitproff
  * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/xtradbrowmarket-cotonti
  * @license BSD
  */
+
 defined('COT_CODE') or die('Wrong URL.');
 require_once cot_incfile('xtradbrowmarket', 'plug');
 
