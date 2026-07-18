9d8567e20f04475809bcfe3e1d60874cc9be3bb6
webitproff <webitproff@gmail.com>
2026-07-19 02:55:07 +0300

    Upgrade xtradbrowmarket to version 3.0.0
    Updated the xtradbrowmarket plugin to version 3.0.0 with multilingual support and new configuration options.

--- xtradbrowmarket/xtradbrowmarket.setup.php
+++ xtradbrowmarket/xtradbrowmarket.setup.php
@@ -2,12 +2,12 @@
 /* ====================
 [BEGIN_COT_EXT]
 Code=xtradbrowmarket
-Name=Custom Extrafields Market
-Description=Custom Extrafields Market for pages in Market PRO v.5 https://github.com/webitproff/marketpro-cotonti
-Version=2.7.9
-Date=Apr 27Th, 2026
+Name=Custom Extrafields Market i18n
+Description=Custom Extrafields Market for pages in Market PRO v.5 with multilingual support. https://github.com/webitproff/marketpro-cotonti
+Version=3.0.0
+Date=Jul 18, 2026
 Author=webitproff
-Copyright=Copyright (c) 2026 webitproff
+Copyright=Copyright (c) 2026 webitproff https://github.com/webitproff/xtradbrowmarket-cotonti
 Notes=BSD License
 Auth_guests=R
 Lock_guests=12345A
@@ -15,23 +15,36 @@ Auth_members=RW
 Lock_members=
 Requires_modules=market
 [END_COT_EXT]
+
+
+[BEGIN_COT_EXT_CONFIG]
+xtradbrowmarket_i18n_use=01:radio::0:Мультиязычность полей активировать и использовать
+xtradbrowmarket_i18n_lang_code_default=02:string::ru:Код основного языка сайта (должен совпадать с <code>$cfg['defaultlang']</code>)
+xtradbrowmarket_i18n_lang_code_first=03:string::en:Код первого дополнительного языка
+xtradbrowmarket_i18n_lang_code_first_use=04:radio::1:Использовать первый дополнительный язык
+xtradbrowmarket_i18n_lang_code_second=05:string::ua:Код второго дополнительного языка
+xtradbrowmarket_i18n_lang_code_second_use=06:radio::1:Использовать второй дополнительный язык
+xtradbrowmarket_i18n_lang_code_third=07:string::pl:Код третьего дополнительного языка
+xtradbrowmarket_i18n_lang_code_third_use=08:radio::0:Использовать третий дополнительный язык
+[END_COT_EXT_CONFIG]
 ==================== */
 
 defined('COT_CODE') or die('Wrong URL');
 
 /**
  * xtradbrowmarket.setup.php - Register data in $db_core and $db_config. Setup & Config File for the Plugin xtradbrowmarket
+ * Filename: plugins/xtradbrowmarket/xtradbrowmarket.extrafields.php
  *
- * xtradbrowmarket plugin for Cotonti 0.9.26, PHP 8.4+
- * Filename: xtradbrowmarket.setup.php
+ *
+ * Custom Extrafields Market i18n plugin for Cotonti v1.+, PHP 8.4+, MySQL 8.4 
  *
  * ReadMeMore:       https://abuyfile.com/ru/market/cotonti/plugs/extrafields-market-custom 
  * Support:          https://abuyfile.com/ru/forums/cotonti/original/extrafields
  * API Extrafields:  https://github.com/Cotonti/Cotonti/blob/master/system/extrafields.php
  *
- * Date: Apr 27Th, 2026
+ * Date: Jul 18, 2026
  * @package xtradbrowmarket
- * @version 2.7.9
+ * @version 3.0.0
  * @author webitproff
  * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/xtradbrowmarket-cotonti
  * @license BSD
