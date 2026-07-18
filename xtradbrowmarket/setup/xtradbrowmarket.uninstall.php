50b41b4c93a99f8b30d343724f05c7533f7af24b
webitproff <webitproff@gmail.com>
2026-07-19 02:47:12 +0300

    Update xtradbrowmarket.uninstall.php

--- xtradbrowmarket/setup/xtradbrowmarket.uninstall.php
+++ xtradbrowmarket/setup/xtradbrowmarket.uninstall.php
@@ -4,10 +4,15 @@
  *
  * Удаляет:
  * - Все записи в таблице cot_extra_fields, относящиеся к таблице $db_xtradbrowmarket
- * - Саму таблицу $db_xtradbrowmarket (DROP TABLE IF EXISTS)
+ * - Таблицу $db_xtradbrowmarket_i18n (переводы)
+ * - Таблицу $db_xtradbrowmarket (основная)
+ * - Папку с загруженными файлами (datas/exflds/xtradbrowmarket)
  *
+ * Custom Extrafields Market i18n plugin for Cotonti v1.+, PHP 8.4+, MySQL 8.4 
+ *
+ * Date: Jul 18, 2026
  * @package xtradbrowmarket
- * @version 2.7.9
+ * @version 3.0.0
  * @author webitproff
  * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/xtradbrowmarket-cotonti
  * @license BSD
@@ -18,10 +23,29 @@ defined('COT_CODE') or die('Wrong URL');
 // Подключаем файл плагина для регистрации глобальных переменных ($db_xtradbrowmarket)
 require_once cot_incfile('xtradbrowmarket', 'plug');
 
-global $db, $db_extra_fields, $db_xtradbrowmarket;
+global $db, $db_extra_fields, $db_xtradbrowmarket, $db_xtradbrowmarket_i18n;
 
 // 1. Удаляем все определения экстраполей для нашей таблицы
 $db->delete($db_extra_fields, "field_location = ?", [$db_xtradbrowmarket]);
 
-// 2. Удаляем саму таблицу (на случай, если SQL-файл не сработал или префикс отличается)
+// 2. Удаляем таблицу переводов (на случай, если SQL-файл не сработал или префикс отличается)
+$db->query("DROP TABLE IF EXISTS `{$db_xtradbrowmarket_i18n}`");
+
+// 3. Удаляем основную таблицу
 $db->query("DROP TABLE IF EXISTS `{$db_xtradbrowmarket}`");
+
+// 4. Удаляем папку с файлами
+$exfld_dir = 'datas/exflds/xtradbrowmarket';
+if (is_dir($exfld_dir)) {
+    // Простая рекурсивная функция удаления директории
+    function xtradbrowmarket_removeDir($dir) {
+        if (!is_dir($dir)) return;
+        $items = array_diff(scandir($dir), ['.', '..']);
+        foreach ($items as $item) {
+            $path = $dir . '/' . $item;
+            is_dir($path) ? xtradbrowmarket_removeDir($path) : unlink($path);
+        }
+        rmdir($dir);
+    }
+    xtradbrowmarket_removeDir($exfld_dir);
+}
