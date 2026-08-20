0ebd4b189ad0628403b2f77f6c9d6104836d2c2b
webitproff <webitproff@gmail.com>
2026-08-21 01:37:59 +0300

    version 4.1.1 from Aug 20Th, 2026
    Updated version and configuration options for multilingual support.

--- xtradbrowmarket/xtradbrowmarket.setup.php
+++ xtradbrowmarket/xtradbrowmarket.setup.php
@@ -2,10 +2,10 @@
 /* ====================
 [BEGIN_COT_EXT]
 Code=xtradbrowmarket
-Name=Custom Extrafields Market i18n
-Description=Custom Extrafields Market for pages in Market PRO v.5 with multilingual support. https://github.com/webitproff/marketpro-cotonti
-Version=4.0.0
-Date=Aug 11Th, 2026
+Name=Extrafields Market Custom i18n
+Description=Extrafields Market Custom for pages in Market PRO v.5 with multilingual support. https://github.com/webitproff/marketpro-cotonti
+Version=4.1.1
+Date=Aug 20Th, 2026
 Author=webitproff
 Copyright=Copyright (c) 2026 webitproff https://github.com/webitproff/xtradbrowmarket-cotonti
 Notes=BSD License
@@ -18,16 +18,17 @@ Requires_modules=market
 
 
 [BEGIN_COT_EXT_CONFIG]
-xtradbrowmarket_i18n_use=01:radio::0:Мультиязычность полей активировать и использовать
+xtradbrowmarket_i18n_use=01:radio::1:Мультиязычность полей активировать и использовать
 xtradbrowmarket_i18n_lang_code_default=02:string::ru:Код основного языка сайта (должен совпадать с <code>$cfg['defaultlang']</code>)
 xtradbrowmarket_i18n_lang_code_first=03:string::en:Код первого дополнительного языка
 xtradbrowmarket_i18n_lang_code_first_use=04:radio::1:Использовать первый дополнительный язык
 xtradbrowmarket_i18n_lang_code_second=05:string::ua:Код второго дополнительного языка
-xtradbrowmarket_i18n_lang_code_second_use=06:radio::1:Использовать второй дополнительный язык
+xtradbrowmarket_i18n_lang_code_second_use=06:radio::0:Использовать второй дополнительный язык
 xtradbrowmarket_i18n_lang_code_third=07:string::pl:Код третьего дополнительного языка
 xtradbrowmarket_i18n_lang_code_third_use=08:radio::0:Использовать третий дополнительный язык
-perpage=09:string::12:Items per page in admin list
-xtradbrowmarket_showallitems=10:radio::0:Show all items (even without extrafields) in admin list
+perpage=09:string::5:Items per page in admin list
+xtradbrowmarket_showallitems=10:radio::1:Show all items (even without extrafields) in admin list
+help_info=11:custom:xtradbrowmarket_setup_help_block()::the reference information block
 [END_COT_EXT_CONFIG]
 ==================== */
 
@@ -38,15 +39,15 @@ defined('COT_CODE') or die('Wrong URL');
  * Filename: plugins/xtradbrowmarket/xtradbrowmarket.extrafields.php
  *
  *
- * Custom Extrafields Market i18n plugin for Cotonti v1.+, PHP 8.5+, MySQL 8.4 
+ * Extrafields Market Custom i18n plugin for Cotonti v1.+, PHP 8.5+, MySQL 8.4 
  *
  * ReadMeMore:       https://abuyfile.com/ru/market/cotonti/plugs/extrafields-market-custom 
  * Support:          https://abuyfile.com/ru/forums/cotonti/original/extrafields
  * API Extrafields:  https://github.com/Cotonti/Cotonti/blob/master/system/extrafields.php
  *
- * Date: Aug 11Th, 2026
+ * Date: Aug 20Th, 2026
  * @package xtradbrowmarket
- * @version 4.0.0
+ * @version 4.1.1
  * @author webitproff
  * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/xtradbrowmarket-cotonti
  * @license BSD
