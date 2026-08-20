044778366aa1d658afa21831e56b72e85a5ee411
webitproff <webitproff@gmail.com>
2026-08-21 01:16:43 +0300

    version 4.1.1 from Aug 20Th, 2026
    Updated plugin metadata and comments in uninstall script.

--- xtradbrowmarket/setup/xtradbrowmarket.uninstall.sql
+++ xtradbrowmarket/setup/xtradbrowmarket.uninstall.sql
@@ -1,6 +1,21 @@
 /* Drops xtradbrowmarket data completely */
 -- Удаление при деинсталляции: plugins/xtradbrowmarket/setup/xtradbrowmarket.uninstall.sql
 -- 
+-- Extrafields Market Custom i18n plugin for Cotonti v1.+, PHP 8.5+, MySQL 8.4 
+-- 
+-- Date: Aug 20Th, 2026
+-- package xtradbrowmarket
+-- version 4.1.1
+-- author webitproff
+-- copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/xtradbrowmarket-cotonti
+-- license BSD
+-- 
+
+DROP TABLE IF EXISTS `cot_xtradbrowmarket_i18n`;
+DROP TABLE IF EXISTS `cot_xtradbrowmarket`;
+/* Drops xtradbrowmarket data completely */
+-- Удаление при деинсталляции: plugins/xtradbrowmarket/setup/xtradbrowmarket.uninstall.sql
+-- 
 -- Custom Extrafields Market i18n plugin for Cotonti v1.+, PHP 8.4+, MySQL 8.4 
 -- 
 -- Date: Jul 18, 2026
