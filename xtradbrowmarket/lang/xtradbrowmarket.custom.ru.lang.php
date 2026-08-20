e4ac78b10785399028754f48d740729e04c04552
webitproff <webitproff@gmail.com>
2026-08-21 01:13:44 +0300

    version 4.1.1 from Aug 20Th, 2026

---
+++ xtradbrowmarket/lang/xtradbrowmarket.custom.ru.lang.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Custom localization file for Cotonti using via function cot_langfile_custom() in system/functions.custom.php
+ * How is works:  https://github.com/webitproff/functions.custom.php-cotonti 
+ * How is works:  https://abuyfile.com/ru/cotonti/reading/rukovodstvo-po-polzovatelskim-funkciyam-cotonti 
+ *
+ * Russian Language File for xtradbrowmarket Plugin with i18n support
+ *
+ * Filename: xtradbrowmarket.custom.ru.lang.php
+ *
+ * Path:    plugins/xtradbrowmarket/lang/xtradbrowmarket.custom.ru.lang.php
+ *
+ * Extrafields Market Custom i18n plugin for Cotonti v1.+, PHP 8.5+, MySQL 8.4
+ *
+ * Source and updates   https://github.com/webitproff/xtradbrowmarket-cotonti
+ * ReadMeMore:          https://abuyfile.com/ru/market/cotonti/plugs/extrafields-market-custom
+ * Support:             https://abuyfile.com/ru/forums/cotonti/original/extrafields
+ * API Extrafields:     https://github.com/Cotonti/Cotonti/blob/master/system/extrafields.php
+ *
+ * Date: Aug 21, 2026
+ *
+ * @package xtradbrowmarket
+ * @version 4.1.1
+ * @author webitproff
+ * @copyright Copyright (c) webitproff 2026 | https://github.com/webitproff/xtradbrowmarket-cotonti
+ * @license BSD
+ */
+defined('COT_CODE') or die('Wrong URL.');
+
+// ===========================
+// Мои локализации заголовков (_TITLE)
+// ===========================
+
+// $L['xtra_field_name_title'] = 'локализованный заголовок/описание экстраполя';
